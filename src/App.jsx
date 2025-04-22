64ef323a0799d7e125237821f87d6efafead8b40
bhaire7 <baibhavnepal07@gmail.com>
2025-04-22 17:49:45 +0545

    Add services and project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Layout from './Layout/Layout';
-import Service from './pages/Service';
-import Blog from './pages/Blog';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Layout from "./Layout/Layout";
+import Service from "./pages/Service";
+import Blog from "./pages/Blog";
+import Project from "./pages/Project";
 
 function App() {
   return (
@@ -15,8 +15,9 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="service" element={<Service />} />
-          <Route path="blog" element = {<Blog />} />
+          <Route path="services" element={<Service />} />
+          <Route path="project" element={<Project />} />
+          <Route path="blog" element={<Blog />} />
         </Route>
       </Routes>
     </Router>
