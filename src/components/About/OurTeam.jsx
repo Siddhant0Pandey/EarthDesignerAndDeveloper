ed5b30329eeb58e25823611089836d8146ea9e83
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-04-29 16:31:22 +0545

    changes in the responsive

--- src/components/About/OurTeam.jsx
+++ src/components/About/OurTeam.jsx
@@ -65,7 +65,7 @@ const OurTeam = () => {
           {/* Team Member 3 */}
           <div className="col-md-4">
             <div className="card h-100 shadow-sm">
-              <img src="public/img/feature-01.jpg" alt="image_not_found" />
+              <img src="/img/feature-01.jpg" alt="image_not_found" />
               <div className="card-body text-center">
                 <h5
                   className="card-title1 text-primary"
