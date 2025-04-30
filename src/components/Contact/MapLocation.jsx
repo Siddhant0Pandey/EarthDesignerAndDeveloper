56f472e878386e015a2f98cd3f15c9f7c9864ad5
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-04-30 17:09:11 +0545

    map and image udpate

--- src/components/Contact/MapLocation.jsx
+++ src/components/Contact/MapLocation.jsx
@@ -3,8 +3,9 @@ function MapLocation() {
   return (
     <section className="gmaps-area">
                 <div className="contact-map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d220.81148776087204!2d85.31713889766534!3d27.686895444105755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19b46db67119%3A0xd7554b8803bba241!2sEarth%20Designers%20%26%20Developers%20Pvt.Ltd!5e0!3m2!1sen!2snp!4v1745741169996!5m2!1sen!2snp" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3605.808707712907!2d85.03535986929846!3d27.430175467283107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb49912e4ef099%3A0x4911789937f7f8dd!2sPurano%20Makawanpur%20Marg%2C%20Hetauda%2044107!5e1!3m2!1sen!2snp!4v1746012039450!5m2!1sen!2snp" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
                 </div>
+             
                 
             </section>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,135 +1,212 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer>
-    <div className="footer-area footer-height pt-40 pb-30">
+      <div className="footer-area footer-height pt-40 pb-30">
         <div className="container">
-            <div className="row">
-                <div className="col-xl-4 col-lg-6 col-md-6">
-                    <div className="footer-left-widget mb-40">
-                        <div className="footer-logo">
-                            <img src="src/assets/img/logo/logo-02.png" alt="logo_not_found"/>
-                        </div>
-                        <p>We are a dedicated team of civil engineers, designers, and developers with over 20 years of experience delivering high-quality construction projects.</p>
-                        <div className="footer-social mt-40">
-                            <Link to="#"><i className="fab fa-twitter"></i></Link>
-                        
-                            <Link to="#"><i className="fab fa-whatsapp"></i></Link>
-                            <Link to="#"><i className="fab fa-facebook"></i></Link>
-                            <Link to="#"><i className="fab fa-instagram"></i></Link>
-                        </div>
-                    </div>
+          <div className="row">
+            <div className="col-xl-4 col-lg-6 col-md-6">
+              <div className="footer-left-widget mb-40">
+                <div className="footer-logo">
+                  <img
+                    src="src/assets/img/logo/logo-22.png"
+                    alt="logo_not_found"
+                  />
                 </div>
-                <div className="col-xl-2 offset-xl-1 col-lg-6 col-md-6">
-                    <div className="footer-widget footer-widget-space mb-40">
-                        <h3>contact us</h3>
-                        <ul className="footer-info">
-                            <li>
-                                <div className="footer-address mt-20">
-                                    <span><i className="fas fa-map-marker-alt"></i></span>
-                                    <h5> M8P8+QW2, Lalitpur 44600</h5>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="footer-address mt-20">
-                                    <span><i className="fas fa-phone-alt"></i></span>
-                                    <h5> +977 985-1213859</h5>
-                                    <h5> +977 985-1213859</h5>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="footer-address mt-20">
-                                    <span><i className="fas fa-envelope-open-text"></i></span>
-                                    <h5>info@yourmail.com</h5>
-                                    <h5>jobsinfo@webmail.com</h5>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+                <p>
+                  We are a dedicated team of civil engineers, designers, and
+                  developers with over 20 years of experience delivering
+                  high-quality construction projects.
+                </p>
+                <div className="footer-social mt-40">
+                  <Link to="#">
+                    <i className="fab fa-twitter"></i>
+                  </Link>
+
+                  <Link to="#">
+                    <i className="fab fa-whatsapp"></i>
+                  </Link>
+                  <Link to="#">
+                    <i className="fab fa-facebook"></i>
+                  </Link>
+                  <Link to="#">
+                    <i className="fab fa-instagram"></i>
+                  </Link>
                 </div>
-                <div className="col-xl-1 offset-xl-1 col-lg-6 col-md-6 pr-0 mb-40">
-                    <div className="footer-widget footer-link">
-                        <h3>Quick links</h3>
-                        <ul>
-                            <li><Link to="/about">About us</Link></li>
-                            <li><Link to="/process">Our process</Link></li>
-                            <li><Link to="/contact">Contact us</Link></li>
-                            <li><Link to="/project">Our Projects</Link></li>
-                            <li><Link to="/blog">Latest News</Link></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="col-xl-2 offset-xl-1 col-lg-6 col-md-6 pr-0">
-                    <div className="footer-widget footer-gallery">
-                        <h3>Work Gallery</h3>
-                        <div className="footer-gallery-item">
-                            <div className="footer-gallery-thumb">
-                                <img src="src/assets/img/footer/footer-gallery/fg-01.jpg" alt="image_not_found"/>
-                            </div>
-                            <div className="link-img">
-                                <Link className="popup-image" to="src/assets/img/footer/footer-gallery/fg-big-01.jpg"></Link>
-                            </div>
-                        </div>
-                        <div className="footer-gallery-item">
-                            <div className="footer-gallery-thumb">
-                                <img src="src/assets/img/footer/footer-gallery/fg-02.jpg" alt="image_not_found"/>
-                            </div>
-                            <div className="link-img">
-                                <Link className="popup-image" to="src/assets/img/footer/footer-gallery/fg-big-02.jpg"></Link>
-                            </div>
-                        </div>
-                        <div className="footer-gallery-item">
-                            <div className="footer-gallery-thumb">
-                                <img src="src/assets/img/footer/footer-gallery/fg-03.jpg" alt="image_not_found"/>
-                            </div>
-                            <div className="link-img">
-                                <Link className="popup-image" to="src/assets/img/footer/footer-gallery/fg-big-03.jpg"></Link>
-                            </div>
-                        </div>
-                        <div className="footer-gallery-item">
-                            <div className="footer-gallery-thumb">
-                                <img src="src/assets/img/footer/footer-gallery/fg-04.jpg" alt="image_not_found"/>
-                            </div>
-                            <div className="link-img">
-                                <Link className="popup-image" to="src/assets/img/footer/footer-gallery/fg-big-04.jpg"></Link>
-                            </div>
-                        </div>
-                        <div className="footer-gallery-item">
-                            <div className="footer-gallery-thumb">
-                                <img src="src/assets/img/footer/footer-gallery/fg-05.jpg" alt="image_not_found"/>
-                            </div>
-                            <div className="link-img">
-                                <Link className="popup-image" to="src/assets/img/footer/footer-gallery/fg-big-05.jpg"></Link>
-                            </div>
-                        </div>
-                        <div className="footer-gallery-item">
-                            <div className="footer-gallery-thumb">
-                                <img src="src/assets/img/footer/footer-gallery/fg-06.jpg" alt="image_not_found"/>
-                            </div>
-                            <div className="link-img">
-                                <Link className="popup-image" to="src/assets/img/footer/footer-gallery/fg-big-06.jpg"></Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
+            <div className="col-xl-2 offset-xl-1 col-lg-6 col-md-6">
+              <div className="footer-widget footer-widget-space mb-40">
+                <h3>contact us</h3>
+                <ul className="footer-info">
+                  <li>
+                    <div className="footer-address mt-20">
+                      <span>
+                        <i className="fas fa-map-marker-alt"></i>
+                      </span>
+                      <h5> M8P8+QW2, Lalitpur 44600</h5>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="footer-address mt-20">
+                      <span>
+                        <i className="fas fa-phone-alt"></i>
+                      </span>
+                      <h5> +977 985-1213859</h5>
+                      <h5> +977 985-1213859</h5>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="footer-address mt-20">
+                      <span>
+                        <i className="fas fa-envelope-open-text"></i>
+                      </span>
+                      <h5>info@yourmail.com</h5>
+                      <h5>jobsinfo@webmail.com</h5>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-xl-1 offset-xl-1 col-lg-6 col-md-6 pr-0 mb-40">
+              <div className="footer-widget footer-link">
+                <h3>Quick links</h3>
+                <ul>
+                  <li>
+                    <Link to="/about">About us</Link>
+                  </li>
+                  <li>
+                    <Link to="/process">Our process</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact">Contact us</Link>
+                  </li>
+                  <li>
+                    <Link to="/project">Our Projects</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">Latest News</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-xl-2 offset-xl-1 col-lg-6 col-md-6 pr-0">
+              <div className="footer-widget footer-gallery">
+                <h3>Work Gallery</h3>
+                <div className="footer-gallery-item">
+                  <div className="footer-gallery-thumb">
+                    <img
+                      src="src/assets/img/footer/footer-gallery/fg-01.jpg"
+                      alt="image_not_found"
+                    />
+                  </div>
+                  <div className="link-img">
+                    <Link
+                      className="popup-image"
+                      to="src/assets/img/footer/footer-gallery/fg-big-01.jpg"
+                    ></Link>
+                  </div>
+                </div>
+                <div className="footer-gallery-item">
+                  <div className="footer-gallery-thumb">
+                    <img
+                      src="src/assets/img/footer/footer-gallery/fg-02.jpg"
+                      alt="image_not_found"
+                    />
+                  </div>
+                  <div className="link-img">
+                    <Link
+                      className="popup-image"
+                      to="src/assets/img/footer/footer-gallery/fg-big-02.jpg"
+                    ></Link>
+                  </div>
+                </div>
+                <div className="footer-gallery-item">
+                  <div className="footer-gallery-thumb">
+                    <img
+                      src="src/assets/img/footer/footer-gallery/fg-03.jpg"
+                      alt="image_not_found"
+                    />
+                  </div>
+                  <div className="link-img">
+                    <Link
+                      className="popup-image"
+                      to="src/assets/img/footer/footer-gallery/fg-big-03.jpg"
+                    ></Link>
+                  </div>
+                </div>
+                <div className="footer-gallery-item">
+                  <div className="footer-gallery-thumb">
+                    <img
+                      src="src/assets/img/footer/footer-gallery/fg-04.jpg"
+                      alt="image_not_found"
+                    />
+                  </div>
+                  <div className="link-img">
+                    <Link
+                      className="popup-image"
+                      to="src/assets/img/footer/footer-gallery/fg-big-04.jpg"
+                    ></Link>
+                  </div>
+                </div>
+                <div className="footer-gallery-item">
+                  <div className="footer-gallery-thumb">
+                    <img
+                      src="src/assets/img/footer/footer-gallery/fg-05.jpg"
+                      alt="image_not_found"
+                    />
+                  </div>
+                  <div className="link-img">
+                    <Link
+                      className="popup-image"
+                      to="src/assets/img/footer/footer-gallery/fg-big-05.jpg"
+                    ></Link>
+                  </div>
+                </div>
+                <div className="footer-gallery-item">
+                  <div className="footer-gallery-thumb">
+                    <img
+                      src="src/assets/img/footer/footer-gallery/fg-06.jpg"
+                      alt="image_not_found"
+                    />
+                  </div>
+                  <div className="link-img">
+                    <Link
+                      className="popup-image"
+                      to="src/assets/img/footer/footer-gallery/fg-big-06.jpg"
+                    ></Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-    <div className="copyright-area pt-20 pb-20">
+      </div>
+      <div className="copyright-area pt-20 pb-20">
         <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="copyright-text text-center">
-                        <p>Developed by <a href='https://www.codesala.com' target='_blank' className='text-warning'>CodeSala</a>. All Rights Reserved.</p>
-                    </div>
-                </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="copyright-text text-center">
+                <p>
+                  Developed by{" "}
+                  <a
+                    href="https://www.codesala.com"
+                    target="_blank"
+                    className="text-warning"
+                  >
+                    CodeSala
+                  </a>
+                  . All Rights Reserved.
+                </p>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</footer>
-  )
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
